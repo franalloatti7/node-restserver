27e3d658da5c868b5b71054d49f79337f5dfac26
Francisco <orealloatti@gmail.com>
2020-03-29 17:03:12 -0300

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,22 @@
 // PUERTO
 // =============================================
 process.env.PORT = process.env.PORT || 3200;
+
+
+// =============================================
+// Entorno
+// =============================================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// =============================================
+// Base de datos
+// =============================================
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://franalloatti:CqsLmqYyaREXUHs9@cluster0-0dcxp.mongodb.net/cafe';
+}
+process.env.URLDB = urlDB;
